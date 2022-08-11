c31aab40fd469f00b94ba34b9c607a56b3148f6c
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2022-08-11 19:24:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/coolmathgame/GameActivity.kt
@@ -0,0 +1,88 @@
+package com.example.coolmathgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
+
+class GameActivity : AppCompatActivity() {
+
+    lateinit var textScore : TextView
+    lateinit var textLife : TextView
+    lateinit var textTime : TextView
+
+    lateinit var textQuestion : TextView
+    lateinit var editTextAnswer : EditText
+
+    lateinit var buttonOK : Button
+    lateinit var buttonNext : Button
+
+    var correctAnswer = 0
+    var userScore = 0
+    var userLife = 3
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
+        textScore = findViewById(R.id.textViewScore)
+        textLife = findViewById(R.id.textViewLife)
+        textTime = findViewById(R.id.textViewTime)
+        textQuestion = findViewById(R.id.textViewQuestion)
+        editTextAnswer = findViewById(R.id.editTextAnswer)
+        buttonOK = findViewById(R.id.buttonOK)
+        buttonNext = findViewById(R.id.buttonNext)
+
+        gameContinue()
+
+        buttonOK.setOnClickListener {
+
+            val input = editTextAnswer.text.toString()
+
+            if (input == "")
+            {
+                    Toast.makeText(applicationContext,"Please write an answer or click the next button",
+                    Toast.LENGTH_LONG).show()
+            }
+            else
+            {
+                val userAnswer = input.toInt()
+
+                if (userAnswer == correctAnswer)
+                {
+                    userScore = userScore + 10
+                    textQuestion.text = "Congratulations, your answer is correct !"
+                    textScore.text = userScore.toString()
+                }
+                else
+                {
+                    userLife--
+                    textQuestion.text = "Sorry, your answer is wrong !"
+                    textLife.text = userLife.toString()
+                }
+            }
+
+        }
+
+        buttonNext.setOnClickListener {
+
+                     gameContinue()
+                     editTextAnswer.setText("")
+             }
+        }
+
+    fun gameContinue()
+    {
+        val number1 = Random.nextInt(0,100)
+        val number2 = Random.nextInt(0,100)
+
+        textQuestion.text = "$number1 + $number2"
+
+        correctAnswer = number1 + number2
+    }
+}
---
+++ app/src/main/java/com/example/coolmathgame/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.coolmathgame
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var addition : Button
+    lateinit var substraction : Button
+    lateinit var multi : Button
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        addition = findViewById(R.id.buttonAdd)
+        substraction = findViewById(R.id.buttonSub)
+        multi = findViewById(R.id.buttonMulti)
+
+        addition.setOnClickListener{
+
+            val intent = Intent(this@MainActivity,GameActivity::class.java)
+            startActivity(intent)
+
+
+        }
+    }
+}
